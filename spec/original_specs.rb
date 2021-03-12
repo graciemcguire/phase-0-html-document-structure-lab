0e198e999c53245115323ce9b8d737efaeba70df
Gracie <m.graciemcguire@gmail.com>
2021-03-12 17:59:26 -0500

    updates specs - ready for review

--- spec/original_specs.rb
+++ spec/original_specs.rb
@@ -1,30 +1,33 @@
-RSpec.describe "Has a Valid Document Structure" do
+RSpec.describe "Well-Formed HTML Document" do
+  it 'has a valid document structure' do
+    expect(parsed_html.children.first).to be_html5_dtd, "Missing DOCTYPE html tag"
+    expect(parsed_html.child.name).to match(/html/i)
 
-  it 'Begins with a DOCTYPE html tag' do
-    expect(html_file_contents).to include('<!DOCTYPE html>'), "Missing DOCTYPE html tag"
-  end
-
-  it 'Includes HTML tags' do
     expect(html_file_contents).to include('<html')
     expect(html_file_contents).to include('</html>')
-  end
 
-  it 'Has a language attribute in the <html> tag' do
-    expect(html_file_contents).to include('<html lang="en">'), 'Include a lang attribute assigned to "en" for English'
-  end
+    head = parsed_html.search('html > head').first
 
-  it 'Properly structures both head and body tags' do
-    # how to check order?
-    # byebug
-    head = parsed_html.search("html > head").first
     expect(head.name).to eq('head')
 
     expect(html_file_contents).to include('</head>')
-
     body = parsed_html.search('html > body').first
+
     expect(body.name).to eq('body')
 
     expect(html_file_contents).to include('</body>')
+
+    validator = W3CValidators::NuValidator.new
+    html = File.read('./index.html')
+    results = validator.validate_text(html)
+
+    error_messages = "Expected a valid w3c document but got:\n#{results.errors.collect{|e| e.to_s}.join("\n")}"
+
+    expect(results.errors).to be_empty, error_messages
+  end
+
+  it 'has a language attribute in the <html> tag' do
+    expect(html_file_contents).to include('<html lang="en">'), 'Include a lang attribute assigned to "en" for English'
   end
 
   context 'within <head>' do
@@ -41,11 +44,7 @@ RSpec.describe "Has a Valid Document Structure" do
       expect(title.name).to eq('title')
 
       expect(html_file_contents).to include('</title>')
-
-      # html_validator
     end
-
   end
 
 end
-  
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -92,5 +92,15 @@ end
 def parsed_html
   Nokogiri::HTML(html_file_contents) do |config|
     config.strict.dtdload.dtdvalid.noblanks
-  end
+end
+
+  # def html_validator
+  #   validator = W3CValidators::NuValidator.new
+  #   html = File.read('./index.html')
+  #   results = validator.validate_text(html)
+
+  #   error_messages = "Expected a valid w3c document but got:\n#{results.errors.collect{|e| e.to_s}.join("\n")}"
+
+  #   expect(results.errors).to be_empty, error_messages
+  # end
 end
